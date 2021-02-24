7d0e0e666d06e20433d1f8a29e29093aa5d42d53
James-Oswald <jamesoswald111@gmail.com>
2021-02-24 00:27:51 -0500

    added createProcess function

--- Process1/src/com/company/Kernel.java
+++ Process1/src/com/company/Kernel.java
@@ -1,27 +1,53 @@
 package com.company;
 
 import java.util.concurrent.Callable;
+import java.util.ArrayList;
 
 public class Kernel{
-    public class PCB{                                   // Process control block. Needs to be filled in.
-        public Callable<?> action;                      // The next code block to run for this process
+
+    //inner class represting a process
+    public class PCB{               // Process control block. Needs to be filled in.
+        public Callable<?> action;  // The next code block to run for this process                    
+        public int pid;
+        public byte[] registers;    // register states
     }
 
-    public GenericHAL hal = new GenericHAL(this);       // the HAL our kernel is running on
-    public PCB current;                                 // the currently running process
+    public PCB current;                            // the currently running process
+    public int pidCounter = 1;                     // the pid of the next created process
+    public GenericHAL hal = new GenericHAL(this);  // the HAL our kernel is running on                
+    public ArrayList<PCB> activeProcesses = new ArrayList<PCB>();
 
-    public void Init(Callable<?> c){                    // called once when the system is started
-        final PCB pcb = new PCB();                      // make a PCB for the Init process
-        pcb.action = c;                                 // Set Init's action
-        current = pcb;                                  // make Init the running process
-        hal.RunCode();                                  // run Init
+    // called once when the system is started
+    public void Init(Callable<?> c){                    
+        final PCB pcb = new PCB();                     
+        pcb.action = c;                               
+        pcb.pid = 0;
+        pcb.registers = new byte[hal.RegisterSize];
+        current = pcb;
+        activeProcesses.add(current);                            
+        hal.RunCode();                            
     }
 
-    public Object Exit() {                              // Exit syscall. Needs more work when we finish processes
+    public int CreateProcess(Callable<?> code){
+        PCB pcb = new PCB();                     
+        pcb.action = code;                               
+        pcb.pid = pidCounter;
+        pcb.registers = new byte[hal.RegisterSize];
+        activeProcesses.add(pcb);
+        pidCounter++;
+        return pcb.pid;
+    }
+
+    public void Reschedule(){
+        
+    }
+
+    // Exit syscall. Needs more work when we finish processes
+    public Object Exit(){                              
         return 0;
     }
 
-    public void Print(String s) {                       // Print a line
-        System.out.println(s);
+    public void Print(String str){                       // Print a line
+        System.out.println(str);
     }
 }

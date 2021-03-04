e3c3d02113965038bf3003a6d5ab23cf2e4354c7
James-Oswald <jamesoswald111@gmail.com>
2021-03-04 02:08:23 -0500

    revamped Main with Kernel Bindings

--- Process1/src/com/company/Main.java
+++ Process1/src/com/company/Main.java
@@ -1,31 +1,30 @@
 package com.company;
 
-import java.util.concurrent.Callable;
-
-public class Main {
-
-    public static class ExitTheThing implements Callable<Void> {
-        public Void call() throws Exception {
-            KernelBindings.Exit(0, null);
-            return null;
-        }
-    }
-
-    public static class PrintTheThing implements Callable<Void> {
-        public Void call() throws Exception {
-            KernelBindings.Print("Code Ran!", new ExitTheThing());
-            return null;
-        }
-    }
+public class Main{
     public static void main(String[] args) {
-        Kernel kernal = new Kernel();
-        kernal.Init(() ->{
-            kernal.CreateProcess(()->KernelBindings.Print("Process 1", ()->KernelBindings.Exit(0, null)));
-            kernal.CreateProcess(()->KernelBindings.Print("Process 2", ()->KernelBindings.Exit(0, null)));
-            kernal.CreateProcess(()->KernelBindings.Print("Process 3", ()->KernelBindings.Exit(0, null)));
-            kernal.CreateProcess(()->KernelBindings.Print("Process 4", ()->KernelBindings.Exit(0, null)));
-            KernelBindings.Exit(0, null);
-            return null;
-        });
+        Kernel kernal = new Kernel();                   //The structure of crating processes is recursive so I outline it once
+        kernal.Init(() ->                               //each parameter gets its own line to make this function hell readable
+            KernelBindings.CreateProcess(               
+                ()->KernelBindings.Print(               //Paramter 1, process 1 code, prints than dies
+                    "Process 1",   
+                    ()->KernelBindings.Exit(0, null)),  //Kill process 1 after it prints
+                ()->KernelBindings.CreateProcess(       //Paramter 2, next action in current (proc 0) process, create another process
+                    ()->KernelBindings.Print(
+                        "Process 2",
+                        ()->KernelBindings.Exit(0, null)), 
+                    ()->KernelBindings.CreateProcess(   
+                        ()->KernelBindings.Print(
+                            "Process 3",
+                            ()->KernelBindings.Exit(0, null)),
+                        ()->KernelBindings.CreateProcess(  
+                            ()->KernelBindings.Print(
+                                "Process 4", 
+                                ()->KernelBindings.Exit(0, null)),
+                            ()->KernelBindings.Exit(0, null)    //final action in process 0, delete our process creation thread.
+                        )
+                    )
+                )
+            )
+        );  
     }
 }
